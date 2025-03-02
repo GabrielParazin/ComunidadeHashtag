5f60411976943da3ea59572663f42cf2a78950b1
GabrielParazin <gv10873@gmail.com>
2025-03-02 12:09:24 -0300

    __init__.py

--- comunidadeimpressionadora/__init__.py
+++ comunidadeimpressionadora/__init__.py
@@ -6,11 +6,19 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+import os
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '70abbb5e00c9f5b1'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'   #URI --> CAMINHO LOCAL
+
+#se o codigo estiver rodando no servidor e estiver online, o bd
+#é database_url, se noa, o bd vai ser o local
+#para manter o codigo tanto pra teste quanto pra prouddçao
+if os.getenv("DATABASE_URL"):       #pra pegar uma varivavel de ambiente
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'   #URI --> CAMINHO LOCAL
 
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
